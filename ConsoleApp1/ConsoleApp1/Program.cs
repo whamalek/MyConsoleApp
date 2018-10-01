20e8a7f0fe334d58e3a175faaedd62e522918e72
W M <waleed.almalik@hotmail.com>
2018-10-01 23:38:26 +0300

    return

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,8 +5,10 @@ namespace ConsoleApp1
     class Program
     {
         //my first app
+
         static void Main(string[] args)
         {
+            return;
             Console.WriteLine("Hello World!");
         }
     }
